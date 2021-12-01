881168dcd9af0f2bf3c89c8354252846fcc6b8a3
Roman Gutovec <roman.gutvec@gmail.com>
2021-12-01 10:38:42 -0800

    Add Basket.API project with entities.

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; }
+        public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+    }
+}
